39d108259008275527b6160132859db2003a4730
Christie Lynam <christielynam@gmail.com>
2017-05-02 13:04:35 -0600

    more JS

--- scripts.js
+++ scripts.js
@@ -1,14 +1,68 @@
 var playerInput = document.querySelector('.player-input');
 
-function zeroState() {
-  generateRandomNumber();
-  playerInput.value = '';
+var guess = parseInt(playerInput.value);
+
+var guessButton = document.getElementById('guess-btn');
+
+var clearButton = document.getElementById('clear-btn');
+
+var resetButton = document.getElementById('reset-btn');
+
+var min;
+
+var max;
+
+var randomNum;
 
 
-}
+window.addEventListener('load', function() {
+    zeroState();
+    generateRandomNumber();
+    playerInput.focus();
+});
+
 
 function generateRandomNumber(min, max) {
   var min = 1;
   var max = 100;
-  Math.floor(Math.random() * (max - min)) + min;
+  randomNum = Math.floor(Math.random() * (max - min)) + min;
+  console.log(randomNum);
+}
+
+guessButton.addEventListener('click', function() {
+  evaluateGuess();
+
+})
+
+function evaluateGuess() {
+  console.log(`guess = ${guess}`);
+  console.log(randomNum);
+  console.log(typeof playerInput.value);
+  debugger;
+  var lastGuess = document.querySelector('.last-guess');
+  var response = document.querySelector('.response');
+  lastGuess.innerText = playerInput.value;
+  if (guess < randomNum) {
+    response.innerText = 'That is too low';
+  } else if (guess > randomNum) {
+    response.innerText = 'That is too high';
+  } else {
+    response.innerText = 'BOOM!'
+  }
+}
+
+clearButton.addEventListener('click', function() {
+  playerInput.value = '';
+})
+
+resetButton.addEventListener('click', function() {
+  zeroState();
+  generateRandomNumber();
+})
+
+function zeroState() {
+  // playerInput.value = '';
+  // guessButton.disabled = true;
+  // clearButton.disabled = true;
+
 }
