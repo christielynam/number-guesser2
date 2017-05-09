665d8f0da3de3f4dec223137f05f0c5512b19757
Christie Lynam <christielynam@gmail.com>
2017-05-09 11:40:53 -0600

    complete phase 3

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-var minimum = document.getElementById('minimum');
+var minRange = document.getElementById('minimum');
 
-var maximum = document.getElementById('maximum');
+var maxRange = document.getElementById('maximum');
 
 var playerInput = document.getElementById('player-input');
 
@@ -8,19 +8,21 @@ var guessButton = document.getElementById('guess-btn');
 
 var clearButton = document.getElementById('clear-btn');
 
-var prevGuess = document.querySelector('.previous-guess');
-
 var lastGuess = document.querySelector('.last-guess');
 
+var currentGuess = document.querySelector('.current-guess');
+
 var response = document.querySelector('.response');
+
+var guessAgain = document.querySelector('.guess-again');
 
 var resetButton = document.getElementById('reset-btn');
 
 var randomNum;
 
 window.addEventListener('load', function() {
-    zeroState();
-    disableButtons();
+  zeroState();
+  disableButtons();
 })
 
 playerInput.addEventListener('input', function() {
@@ -28,8 +30,8 @@ playerInput.addEventListener('input', function() {
 })
 
 guessButton.addEventListener('click', function() {
-  var min = parseInt(minimum.value);
-  var max = parseInt(maximum.value);
+  var min = parseInt(minRange.value);
+  var max = parseInt(maxRange.value);
   var guess = parseInt(playerInput.value);
   generateRandomNumber(min, max);
   evaluateInput(guess, min, max);
@@ -46,74 +48,101 @@ resetButton.addEventListener('click', function() {
   zeroState();
   enableRange();
   disableButtons();
-  minimum.value = '1';
-  maximum.value = '100';
+  minRange.value = '1';
+  maxRange.value = '100';
 })
 
 function zeroState() {
   playerInput.value = '';
   playerInput.focus();
-  prevGuess.textContent = 'Your last guess was';
-  lastGuess.textContent = '?';
+  lastGuess.textContent = 'Your last guess was';
+  currentGuess.textContent = '?';
   response.textContent = 'Too High/Too Low';
   randomNum = null;
+  resetButton.disabled = true;
 }
 
-function generateRandomNumber(minimum, maximum) {
+function generateRandomNumber(min, max) {
   if (!randomNum) {
-    var min = Math.ceil(minimum);
-    var max = Math.floor(maximum);
+    var min = Math.ceil(min);
+    var max = Math.floor(max);
     randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
     disableRange();
-    console.log( "randomNumber" + randomNum);
+    console.log( "randomNum" + randomNum);
   }
 }
 
-function disableRange() {
-  minimum.disabled = true;
-  maximum.disabled = true;
+function adjustRange(min, max) {
+  var min = parseInt(minRange.value);
+  var max = parseInt(maxRange.value);
+  minRange.value = min - 10;
+  maxRange.value = max + 10;
+  console.log('min', min - 10);
+  console.log('max', max + 10);
 }
 
 function enableRange() {
-  minimum.value = '';
-  maximum.value = '';
-  minimum.disabled = false;
-  maximum.disabled = false;
+  minRange.value = '1';
+  maxRange.value = '100';
+  minRange.disabled = false;
+  maxRange.disabled = false;
+  resetButton.disabled = true;
 }
 
-function evaluateInput(guess, minimum, maximum) {
-  if (guess < minimum || guess > maximum || isNaN(guess) || isNaN(minimum) || isNaN(maximum)) {
-    lastGuess.textContent = 'Invalid Entry';
+function disableRange() {
+  minRange.disabled = true;
+  maxRange.disabled = true;
+  resetButton.disabled = false;
+}
+
+function evaluateInput(guess, min, max) {
+  if (guess < min || guess > max || isNaN(guess) || isNaN(min) || isNaN(max)) {
+    currentGuess.textContent = 'Invalid Entry';
     playerInput.value = '';
     playerInput.focus();
   } else {
-    lastGuess.textContent = guess;
+    currentGuess.textContent = guess;
     evaluateGuess(guess);
   }
 }
 
 function evaluateGuess(guess) {
   if (guess < randomNum) {
-    response.textContent = 'That is too low';
+    tooLow();
   } else if (guess > randomNum) {
-    response.textContent = 'That is too high';
+    tooHigh();
   } else {
-    lastGuess.textContent = 'BOOM!';
-    prevGuess.textContent = '';
-    response.textContent = '';
+    boom();
+    adjustRange();
     randomNum = null;
-    //TODO call a fx that adjusts the min and max range
   }
+}
+
+function tooLow() {
+  lastGuess.textContent = 'Your last guess was'
+  response.textContent = 'That is too low';
+  guessAgain.textContent = '';
+}
+
+function tooHigh() {
+  lastGuess.textContent = 'Your last guess was'
+  response.textContent = 'That is too high';
+  guessAgain.textContent = '';
+}
+
+function boom() {
+  currentGuess.textContent = 'BOOM!';
+  lastGuess.textContent = 'Youre good... Lets make this a little harder!';
+  response.textContent = 'Decreasing the min by 10, increasing the max by 10...';
+  guessAgain.textContent = 'Now Guess Again!';
 }
 
 function disableButtons() {
   if (playerInput.value === '') {
     guessButton.disabled = true;
     clearButton.disabled = true;
-    resetButton.disabled = true;
   } else {
     guessButton.disabled = false;
     clearButton.disabled = false;
-    resetButton.disabled = false;
   }
 }
